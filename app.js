cb6f52b522a800889cc28c74cf8124cdf7dfbff9
FrancisCKY <a231685600@gmail.com>
2023-09-06 23:02:49 +0800

    implement basic setting

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+app.use(adminRoutes)
+app.use(shopRoutes)
+
+app.listen(3000)
