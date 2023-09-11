8604d1011595837f47b0194ff2c2f521bacb3e14
FrancisCKY <a231685600@gmail.com>
2023-09-11 09:11:22 +0800

    implement handlebars

--- routes/admin.js
+++ routes/admin.js
@@ -13,7 +13,6 @@ router.get('/add-product', (req, res) => {
 router.post('/add-product', (req, res, next) => {
   products.push({ title: req.body.title })
   res.redirect('/')
-  next()
 })
 
 exports.routes = router;
