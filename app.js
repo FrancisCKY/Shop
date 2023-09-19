4325d319f4718028ebddec5564506fd3a5146442
FrancisCKY <a231685600@gmail.com>
2023-09-19 12:08:22 +0800

    Syncing JS Definitions to the Database

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const bodyParser = require('body-parser')
-const db = require('./util/database')
+const sequelize = require('./util/database')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 const errorController = require('./controllers/error')
@@ -17,4 +17,11 @@ app.use(shopRoutes)
 
 app.use(errorController.get404)
 
-app.listen(3000)
+sequelize
+  .sync()
+  .then((result) => {
+    app.listen(3000)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
