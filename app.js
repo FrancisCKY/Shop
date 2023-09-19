d3488498e935eac65d7ccd73eec2b9adb0e8b8fd
FrancisCKY <a231685600@gmail.com>
2023-09-19 16:32:12 +0800

    Creating & Managing a Dummy User

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ app.set('views', './views');
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user
+      next()
+    })
+    .catch((err) => console.log(err))
+})
+
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
@@ -23,8 +33,18 @@ Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' })
 User.hasMany(Product)
 
 sequelize
-  .sync({ force: true })
+  // .sync({ force: true })
+  .sync()
   .then((result) => {
+    return User.findByPk(1)
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@gmail.com' })
+    }
+    return user
+  })
+  .then(user => {
     app.listen(3000)
   })
   .catch((err) => {
