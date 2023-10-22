6fbc462381c4f8b0c7d51bd548e527cebce20238
FrancisCKY <a231685600@gmail.com>
2023-10-22 15:45:45 +0800

    Add & Using User model

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 const errorController = require('./controllers/error')
+const User = require('./models/user')
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -13,16 +14,16 @@ app.set('views', './views');
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.use((req, res, next) => {
-//   User.findById("651d332e84fa474f079480f2")
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id)
-//       next()
-//     })
-//     .catch(err => {
-//       console.log(err)
-//     })
-// })
+app.use((req, res, next) => {
+  User.findById("6534d13d732042a42d33e5e7")
+    .then(user => {
+      req.user = user
+      next()
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
@@ -32,6 +33,18 @@ app.use(errorController.get404)
 mongoose
   .connect('mongodb+srv://francis:mongo123@cluster0.l9tmhqb.mongodb.net/shop?retryWrites=true')
   .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: "Peter",
+          email: "132@gmail.com",
+          cart: {
+            items: []
+          }
+        })
+        user.save()
+      }
+    })
     app.listen(3000)
   })
   .catch(err => {
